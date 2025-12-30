a9e26fae17137f2598c81a187083c53e966e6c86
enzhe-r <njnjnzh@gmail.com>
2025-12-30 14:52:11 +0300

    Delete SQL_BOOTCAMP/SQL_day06 directory

--- SQL_BOOTCAMP/SQL_day06/day05_ex01.sql
+++
@@ -1,4 +0,0 @@
-SET enable_seqscan TO OFF;
-EXPLAIN ANALYZE
-SELECT m.pizza_name, pz.name AS pizzeria_name FROM menu m
-INNER JOIN pizzeria pz ON m.pizzeria_id = pz.id;
--- SQL_BOOTCAMP/SQL_day06/day05_ex04.sql
+++
@@ -1,6 +0,0 @@
-CREATE UNIQUE INDEX idx_menu_unique ON menu(pizzeria_id, pizza_name);
-
-SET enable_seqscan TO OFF;
-EXPLAIN ANALYZE
-SELECT pizzeria_id, pizza_name FROM menu
-    WHERE pizza_name = 'supreme pizza' AND pizzeria_id = 1;
