a6e5807c1d2c7e1a97b43646b65190f350256488
enzhe-r <njnjnzh@gmail.com>
2025-12-30 15:00:06 +0300

    Add files via upload

---
+++ SQL_BOOTCAMP/SQL_day08/day08_ex06.sql
@@ -0,0 +1,14 @@
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- Session #2
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+-- Session #1
+SELECT sum(rating) FROM pizzeria;
+-- Session #2
+UPDATE pizzeria SET rating = 5 WHERE name = 'Pizza Hut';
+COMMIT;
+-- Session #1
+SELECT sum(rating) FROM pizzeria;
+COMMIT;
+SELECT sum(rating) FROM pizzeria;
+-- Session #2
+SELECT sum(rating) FROM pizzeria;
